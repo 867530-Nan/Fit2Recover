2b44f6fa68e8fb8f9db9fae695b3cf77e49b80e8
Matthew Peterson <mbpeterson19@gmail.com>
2017-08-30 01:49:33 -0600

    finally it works

--- app/controllers/api/blogs_controller.rb
+++ app/controllers/api/blogs_controller.rb
@@ -1,9 +1,8 @@
 class Api::BlogsController < ApplicationController
 
   def index
-  	passcode = ENV['BLOGGER_API_KEY']
-  	blog = ENV['BLOG_ID']
-  	nutrition = HTTParty.get("https://www.googleapis.com/blogger/v3/blogs/#{blog}/posts?fetchImages=true&maxResults=10&orderBy=published&key=#{passcode}")
+  	nutrition = HTTParty.get("https://www.googleapis.com/blogger/v3/blogs/#{ENV['BLOG_ID']}/posts?fetchImages=true&maxResults=10&orderBy=published&key=#{ENV['BLOGGER_API_KEY']}")
+
       render json: nutrition
   end
 end
